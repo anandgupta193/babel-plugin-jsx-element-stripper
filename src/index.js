65ef30038abf9a4334b288a96631a3949a6a22d0
Anand Gupta <anandgupta@197NODMB21115.local>
2018-07-25 14:52:28 +0530

    Adding Initial Files for Plugin

---
+++ src/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+exports.default = function (babel) {
+    var t = babel.types;
+
+
+    return {
+        visitor: {
+            Program: function Program(path, state) {
+                state.classMethods = {};
+                Object.assign(state, {
+                    mainPlace: path,
+                    classMethods: {}
+                });
+            },
+            Class: function (path, state) {
+                path.traverse({
+                    ClassMethod: function ClassMethod(path) {
+                        state.classMethods[path.node.key.name] = path;
+                    }
+                });
+
+                Object.keys(state.classMethods).forEach(element => {
+                    if (element === 'render') {
+
+                        console.log(element);
+
+                        // state.mainPlace.replaceWith(t.Program([t.expressionStatement(renderReturn)]));
+                    }
+                });
+            }
+        }
+    };
+};
